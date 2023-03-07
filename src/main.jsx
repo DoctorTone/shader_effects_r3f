b5bcab204f7fbfd62587401da87717d44b167b45
DRT Software <tony@drt-software.com>
2023-03-07 01:07:29 +0000

    Configure glsl

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/material/styles';
-import theme from './styles/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ThemeProvider theme={theme}>
+  <>
     {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-    <CssBaseline />
     <App />
-  </ThemeProvider>,
+  </>
 )
