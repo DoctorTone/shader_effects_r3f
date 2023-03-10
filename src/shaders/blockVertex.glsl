0c9eb08d4dcc54189e04292aa1dc05dd5134958e
DoctorTone <drtonesoftware@gmail.com>
2023-03-10 16:05:45 +0000

    Animate all upper vertices

--- src/shaders/blockVertex.glsl
+++ src/shaders/blockVertex.glsl
@@ -6,9 +6,9 @@ uniform float frequency;
 
 void main() {
     LocalPos = position;
-    if (LocalPos.x > 0.0 && LocalPos.y > 0.0) {
+    if (LocalPos.y > 0.0) {
         LocalPos.z += amplitude * sin(time * frequency);
-        LocalPos.y += amplitude * sin(time * frequency);
+        LocalPos.y += amplitude * cos(time * frequency);
     }
     vUv = uv;
     gl_Position = projectionMatrix * modelViewMatrix * vec4(LocalPos, 1.0);
