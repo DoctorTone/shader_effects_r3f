d100a4e277c74b61ffca5697bdffc8f5924b3486
DoctorTone <drtonesoftware@gmail.com>
2023-03-13 23:16:10 +0000

    Configure top ring

--- src/components/Rings.jsx
+++ src/components/Rings.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import { Torus } from "@react-three/drei";
 
 const Rings = () => {
-  return <Torus />;
+  return (
+    <Torus args={[1, 0.1]} rotation-x={Math.PI / 3}>
+      <meshStandardMaterial color="hotpink" />
+    </Torus>
+  );
 };
 
 export default Rings;
