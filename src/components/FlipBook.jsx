c7452d25a954fe113b41f696552dbc266dd782a4
DoctorTone <drtonesoftware@gmail.com>
2023-03-13 23:51:54 +0000

    Add plane for flip book

---
+++ src/components/FlipBook.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Plane } from "@react-three/drei";
+
+const FlipBook = () => {
+  return <Plane material-color="black" args={[3, 3]} />;
+};
+
+export default FlipBook;
--- src/effects/Effect.jsx
+++ src/effects/Effect.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import BlockAnimation from "../shaders/BlockAnimation.jsx";
 import { Sphere } from "@react-three/drei";
-import { Tube } from "@react-three/drei";
 import Rings from "../components/Rings.jsx";
+import FlipBook from "../components/FlipBook.jsx";
 
 const Effect = ({ name }) => {
   return (
     <>
       {name === "block" && <BlockAnimation />}
       {name === "pattern" && <Sphere />}
-      {name === "flip" && <Tube />}
+      {name === "flip" && <FlipBook />}
       {name === "rings" && <Rings />}
     </>
   );
