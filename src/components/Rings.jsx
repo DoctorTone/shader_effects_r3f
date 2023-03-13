0fa36cdbfd72b43b9da96b2a6f0bd985ca0df535
DoctorTone <drtonesoftware@gmail.com>
2023-03-13 23:40:15 +0000

    Add two spinning rings

--- src/components/Rings.jsx
+++ src/components/Rings.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Torus } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
 
 const Rings = () => {
+  const groupRef = useRef();
+
+  useFrame((state, delta) => {
+    groupRef.current.rotation.y += delta * 4;
+  });
+
   return (
-    <Torus args={[1, 0.1]} rotation-x={Math.PI / 3}>
-      <meshStandardMaterial color="hotpink" />
-    </Torus>
+    <group position-y={1.5} ref={groupRef}>
+      <Torus args={[1, 0.1]} rotation-x={Math.PI / 3}>
+        <meshStandardMaterial color="hotpink" />
+      </Torus>
+      <Torus args={[1, 0.1]} position-y={-1.15} rotation-x={-Math.PI / 3}>
+        <meshStandardMaterial color="hotpink" />
+      </Torus>
+    </group>
   );
 };
 
