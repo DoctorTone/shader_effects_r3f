b4714123630a3ccd699855bd4594eb11b93eecb8
DoctorTone <drtonesoftware@gmail.com>
2023-03-17 15:09:28 +0000

    Add flipbook shader framework

--- src/shaders/FlipBook.jsx
+++ src/shaders/FlipBook.jsx
@@ -1,8 +1,20 @@
 import React from "react";
-import { Plane } from "@react-three/drei";
+import { shaderMaterial } from "@react-three/drei";
+import { extend, useFrame } from "@react-three/fiber";
+import flipVertexShader from "./flipVertex.glsl";
+import flipFragmentShader from "./flipFragment.glsl";
 
 const FlipBook = () => {
-  return <Plane material-color="black" args={[3, 3]} />;
+  const FlipMaterial = shaderMaterial({}, flipVertexShader, flipFragmentShader);
+
+  extend({ FlipMaterial });
+
+  return (
+    <mesh>
+      <planeGeometry args={[3, 3]} />
+      <flipMaterial />
+    </mesh>
+  );
 };
 
 export default FlipBook;
