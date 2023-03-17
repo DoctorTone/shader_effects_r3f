8de1eb613b16c9bfb704a130ecaeecf10135989a
DoctorTone <drtonesoftware@gmail.com>
2023-03-17 16:33:30 +0000

    Implemented flipbook shader

--- src/shaders/FlipBook.jsx
+++ src/shaders/FlipBook.jsx
@@ -1,18 +1,51 @@
-import React from "react";
-import { shaderMaterial } from "@react-three/drei";
+import React, { useRef } from "react";
+import * as THREE from "three";
+import { shaderMaterial, useTexture } from "@react-three/drei";
 import { extend, useFrame } from "@react-three/fiber";
 import flipVertexShader from "./flipVertex.glsl";
 import flipFragmentShader from "./flipFragment.glsl";
 
+const FLIP_TIME = 0.01;
+const TEXTURE_WIDTH = 5;
+const TEXTURE_HEIGHT = 5;
+
 const FlipBook = () => {
-  const FlipMaterial = shaderMaterial({}, flipVertexShader, flipFragmentShader);
+  const flipTexture = useTexture("./src/assets/fire.jpg");
+
+  const materialRef = useRef();
+  const elapsedTime = useRef(0);
+  const currentCell = useRef(new THREE.Vector2());
+
+  const FlipMaterial = shaderMaterial(
+    {
+      cell: new THREE.Vector2(),
+      map: new THREE.Texture(),
+    },
+    flipVertexShader,
+    flipFragmentShader
+  );
+
+  useFrame((state, delta) => {
+    elapsedTime.current += delta;
+    if (elapsedTime.current >= FLIP_TIME) {
+      if (++currentCell.current.x > TEXTURE_WIDTH) {
+        currentCell.current.x = 0;
+        if (++currentCell.current.y > TEXTURE_HEIGHT) {
+          currentCell.current.y = 0;
+        }
+      }
+      materialRef.current.cell.x = currentCell.current.x;
+      materialRef.current.cell.y = currentCell.current.y;
+      elapsedTime.current = 0;
+    }
+  });
 
   extend({ FlipMaterial });
 
   return (
     <mesh>
       <planeGeometry args={[3, 3]} />
-      <flipMaterial />
+      <flipMaterial ref={materialRef} map={flipTexture} />
     </mesh>
   );
 };
--- src/shaders/flipFragment.glsl
+++ src/shaders/flipFragment.glsl
@@ -1,6 +1,11 @@
 varying vec2 vUv;
+uniform sampler2D map;
+uniform vec2 cell;
+vec2 texTransform;
 
 void main()
 {
-    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    texTransform = (vUv / 6.0) + (cell / 6.0);
+    vec3 diffuseColor = texture2D( map, texTransform).rgb;
+    gl_FragColor = vec4(diffuseColor, 1.0);
 }
